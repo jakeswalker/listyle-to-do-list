5b1aecb5152bb9b87ab0aee5638924971a965f3a
Jake Walker <122703805+jakeswalker@users.noreply.github.com>
2025-03-21 22:27:07 -1000

    Add AppDelegate

---
+++ Listyle/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Listyle
+//
+//  Created by Jake Walker on 3/21/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
