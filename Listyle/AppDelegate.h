938a25d0f36b2808c4dd6b66a49aee8dfad24ce8
Jake Walker <122703805+jakeswalker@users.noreply.github.com>
2025-03-22 16:09:59 -1000

    Update README

---
+++ Listyle/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Listyle
+//
+//  Created by Jake Walker on 3/22/25.
+//
+//  The full source code is available upon request.
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
